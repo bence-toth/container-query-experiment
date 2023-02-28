244078eb34d5eb0b542b233cda8cb39a63727d2e
Bence A. Tóth <tothab@gmail.com>
2023-02-28 11:51:36 +0100

    Created smaller components

--- src/App.tsx
+++ src/App.tsx
@@ -1,95 +1,12 @@
+import Resizable from "./Resizable";
+import Poem from "./Poem";
+
 import "./App.css";
 
 const App = () => (
   <div className="app-container">
-    <div className="left-container">
-      <div className="resize-marker">
-        Resize me <span className="arrow">↘</span>
-      </div>
-    </div>
-    <div className="right-container">
-      <article className="poem">
-        <header>
-          <h2>Ode to container queries</h2>
-          <p className="author">by ChatGPT</p>
-        </header>
-        <div className="verses">
-          <p>
-            Container queries, how you excite
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;us web developers day and night.
-            <br />
-            No more hard-coded, fixed breakpoints.
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;Now we can target containers with finesse.
-          </p>
-          <p>
-            Design systems will be much more fluid
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;as we adjust for each container’s width.
-            <br />
-            Our layouts will adapt seamlessly
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;to screens of all shapes and sizes,
-            endlessly.
-          </p>
-          <p>
-            We’ll use flexbox and grid with ease
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;to craft designs that flow like the breeze,
-            <br />
-            containers will respond like magic
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;to the content that we craft so fantastic.
-          </p>
-          <p>
-            Gone are the days of media queries galore
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;that often left us with an eyesore.
-            <br />
-            Now we can style with precision and glee.
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;Container queries, you set us free.
-          </p>
-          <p>
-            Our designs will be more accessible,
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;and layouts more intelligible.
-            <br />
-            We’ll focus on the content, not the screen
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;thanks to container queries, our new regime.
-          </p>
-          <p>
-            No more workarounds, no more strife.
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;Container queries will change our life.
-            <br />
-            Our code will be cleaner and more refined
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;as we build layouts that are truly divine.
-          </p>
-          <p>
-            So let’s celebrate this paradigm shift
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;and make container queries our go-to gift
-            <br />
-            for building responsive websites with grace
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;and ensuring the user experience we embrace.
-          </p>
-          <p>
-            Thank you, container queries, for your charm
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;and for keeping us from any harm.
-            <br />
-            As we craft the web with beauty and flair,
-            <br />
-            &nbsp;&nbsp;&nbsp;&nbsp;we’ll know that you will always be there.
-          </p>
-        </div>
-      </article>
-    </div>
+    <Resizable />
+    <Poem />
   </div>
 );
 
---
+++ src/Resizable.tsx
@@ -0,0 +1,11 @@
+import "./Resizable.css";
+
+const Resizable = () => (
+  <div className="resizable">
+    <div className="resize-marker">
+      Resize me <span className="arrow">↘</span>
+    </div>
+  </div>
+);
+
+export default Resizable;
